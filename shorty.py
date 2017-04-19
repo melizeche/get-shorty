f9a151ba0c77c6c641807dde7c8ec6c4fa43cd71
Marcelo Elizeche Landó <melizeche@gmail.com>
2017-04-19 03:55:14 -0400

    Added redirects counter to the db and hit() method for those counters, added docstrings to every method, code cleanup and rename app.py to shorty.py

--- shorty.py
+++ shorty.py
@@ -10,13 +10,17 @@ HOST = 'http://localhost:5000'
 
 
 def create_table():
+    """ Initialize the DB """
     query = """
         create table urls (
             id integer primary key autoincrement,
             short text unique not null,
             default_url text not null,
+            default_hits integer default 0,
             mobile_url text,
+            mobile_hits integer default 0,
             tablet_url text,
+            tablet_hits integer default 0,
             created datetime default current_timestamp
         );"""
     with sq.connect('getshorty.sqlite3') as conn:
@@ -28,23 +32,61 @@ def create_table():
 
 
 def get_max_id():
+    """
+    Get the max id in the DB to be able to encode the next one.
+
+    :returns: the value+1 of the last autoincremented id
+    :raises OperationalError: raises an exception if there's a problem with the DB
+    """
     query = """
         SELECT MAX(id) from urls;
         """
     with sq.connect('getshorty.sqlite3') as conn:
+        conn.row_factory = sq.Row
         cursor = conn.cursor()
         try:
             cursor.execute(query)
             result = cursor.fetchone()
+            print('{}'.format(result))
+            print(type(result))
             max_id = 0 if result[0] == None else result[0]
-            print(max_id)
-            print(type(max_id))
             return max_id + 1
         except sq.OperationalError:
             print("Error!")
 
 
+def hit(urlcode, target):
+    """
+    Sum +1 to the redirects/hits counter in the db
+
+    :param urlcode: the shorten urlcode
+    :param target: the target choosen for add 1 hit to the counter
+    :returns: True if was succesful, otherwise False
+    :raises OperationalError: raises an exception if there's a problem with the DB
+    """
+    column = dict(default='default_hits',
+                  mobile='mobile_hits', tablet='tablet_hits')
+    query = 'UPDATE urls SET {target_hits} = {target_hits} + 1 WHERE short = {urlcode};'.\
+            format(target_hits=column[target], urlcode=urlcode)
+    print(query)
+    with sq.connect('getshorty.sqlite3') as conn:
+        cursor = conn.cursor()
+        try:
+            cursor.execute(query)
+            return True
+        except sq.OperationalError:
+            print("ERROR")
+            return False
+
+
 def valid_url(url):
+    """
+    Validates that the url provided is in a valid format.
+
+    :param url: an url
+    :returns: True if it is a valid url, False otherwise
+    :raises OperationalError: raises an exception if there's a problem with the DB
+    """
     parsed = urlparse(url)
     if parsed.scheme and parsed.netloc:
         return True
@@ -52,12 +94,19 @@ def valid_url(url):
 
 
 @app.route('/api/1.0/create', methods=['POST'])
-def test():
+def create():
+    """
+    Endpoint to create a short url and optional specify some targets(mobile, tablet) 
+
+    :param url: The default url to be shorten, target=Desktop/default  
+    :param url-mobile: [Optional] The url to redirect if the client is a mobile device 
+    :param url-tablet: [Optional] The url to redirect if the client is a tablet
+    :returns: JSON, params = shorten, url , mobile, tablet
+    :raises OperationalError: raises an exception if there's a problem with the DB
+    """
     url, mobile_url, tablet_url = None, None, None
     errors = []
     params = request.get_json(force=True)
-    # ua = request.headers.get('User-Agent')
-    # print(ua)
     print(params)
     if 'url' in params and params['url']:
         url = params['url']
@@ -68,13 +117,10 @@ def test():
             mobile_url = params['url-mobile']
             if not valid_url(params['url-mobile']):
                 errors.append({'detail': 'Invalid url url-mobile'})
-            print(mobile_url)
         if 'url-tablet' in params and params['url-tablet']:
             tablet_url = params['url-tablet']
             if not valid_url(params['url-tablet']):
                 errors.append({'detail': 'Invalid url url-tablet'})
-            print(tablet_url)
-        print(url, mobile_url, tablet_url)
         if not errors:
             shorten = long_to_short(url, mobile_url, tablet_url)
             if not shorten:
@@ -86,14 +132,22 @@ def test():
         errors.append({'detail': 'Missing url parameter'})
     return Response(json.dumps(dict(errors=errors)), status=422, mimetype="application/json")
 
+
 def dict_factory(cursor, row):
     d = {}
     for idx, col in enumerate(cursor.description):
         d[col[0]] = row[idx]
     return d
 
+
 @app.route('/api/1.0/list', methods=['GET'])
 def get_urls():
+    """
+    Lists all urls with the targets and redirects/hits counter
+
+    :returns: JSON, all the data in the urls table
+    :raises: raises an exception if there's a problem with the DB
+    """
     query = 'SELECT * from urls;'
     print(query)
     with sq.connect('getshorty.sqlite3') as conn:
@@ -105,20 +159,28 @@ def get_urls():
             print("AAAAAA")
             for row in rows:
                 short = row['short']
-                row['short'] = '{host}/{short}'.format(host=HOST,short=row['short'])
+                row['short'] = '{host}/{short}'.format(
+                    host=HOST, short=row['short'])
             return Response(json.dumps(rows), status=200, mimetype="application/json")
         except:
             return Response(json.dumps(dict(errors='Server Error')), status=500, mimetype="application/json")
 
 
 def long_to_short(url, url_mobile=None, url_tablet=None):
+    """
+    Create a code for the shortened url and inserts in the DB along with the targets 
+    Uses base36 enconding so only numbers and lowercase characters for better usability
+
+    :param url: The default url to be shorten, target=Desktop/default  
+    :param url_mobile: [Optional] The url to redirect if the client is a mobile device 
+    :param url_tablet: [Optional] The url to redirect if the client is a tablet
+    :returns: String, base_id=the short code 
+    :raises OperationalError: raises an exception if there's a problem with the DB
+    """
     url_id = get_max_id()
-    print(url_id)
     based_id = base36.encode(url_id)
-    print(based_id)
     query = 'INSERT into urls(short,default_url,mobile_url,tablet_url) VALUES ("{short}","{url}","{mobile}","{tablet}");'.\
         format(short=based_id, url=url, mobile=url_mobile, tablet=url_tablet)
-    print(query)
     with sq.connect('getshorty.sqlite3') as conn:
         cursor = conn.cursor()
         try:
@@ -130,38 +192,48 @@ def long_to_short(url, url_mobile=None, url_tablet=None):
 
 
 def short_to_long(urlcode):
+    """
+    Get the the long url, targets and data for the urlcode provided.
+
+    :param urlcode: the code for the shorten url  
+    :returns: sqlite.Row, DB row
+    :raises: raises an exception if there's a problem with the DB
+    """
     query = 'SELECT * from urls where short="{short}";'.format(short=urlcode)
-    print(query)
     with sq.connect('getshorty.sqlite3') as conn:
         conn.row_factory = sq.Row
         cursor = conn.cursor()
-        # try:
-        cursor.execute(query)
-        row = cursor.fetchone()
-        print("AAAAAA")
-        #print(row)
-        return row
-        # except:
-        #     print("ERROR")
+        try:
+            cursor.execute(query)
+            row = cursor.fetchone()
+            return row
+        except:
+            return False
 
 
 @app.route('/<string:urlcode>', methods=['GET'])
 def lookup(urlcode):
-    print(urlcode)
+    """
+    Endpoint that recieves the short url and redirects to the long url target
+    Case insensitive for better usability 
+
+    :param urlcode: The code of the short url  
+    :returns: redirects to target
+    """
+    urlcode = urlcode.lower()
     result = short_to_long(urlcode)
     print(type(result))
     if result:
         url = result['default_url']
+        target = 'default'
         ua = parse_ua(request.headers.get('User-Agent'))
-        print(ua, ua.is_mobile, ua.is_tablet)
-        print(result.keys())
-        print(result['mobile_url'])
-        print('true' if 'mobile_url' in result else 'false')
         if result['mobile_url'] and ua.is_mobile:
             url = result['mobile_url']
+            target = 'mobile'
         elif result['tablet_url'] and ua.is_tablet:
             url = result['tablet_url']
-        print(url)
+            target = 'tablet'
+        hit(urlcode, target)
         return redirect(url)
         return Response(str(result['mobile_url ']))
     return Response("URL NOT FOUND", status=404)
@@ -169,5 +241,4 @@ def lookup(urlcode):
 
 if __name__ == '__main__':
     create_table()
-    # print(get_max_id())
     app.run(debug=True)
